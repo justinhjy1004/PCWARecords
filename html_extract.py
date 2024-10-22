efea5edc51b55170527cc4d3314d1566cca56191
justinhjy1004 <justinhjy1004@gmail.com>
2024-10-22 14:01:15 -0400

    quite substantial changes

--- html_extract.py
+++ html_extract.py
@@ -3,10 +3,29 @@ import re
 import logging
 import datetime
 import pandas as pd
+import pytesseract
+from pdf2image import convert_from_path
+from PIL.Image import DecompressionBombError
 import sqlite3
+import os
+from zipfile import ZipFile 
 LOG_FILENAME = 'html_extract.log'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 
+def convert_to_text(pdf):
+
+    try:
+        images = convert_from_path(pdf)
+        text = ""
+
+        for image in images:
+            text += pytesseract.image_to_string(image)
+
+        return text
+
+    except DecompressionBombError:
+        with open('ocred.log', "a") as log_file:
+            log_file.write(f"{pdf} is too big!")
 
 if __name__ == '__main__':
 
@@ -16,116 +35,135 @@ if __name__ == '__main__':
     con = sqlite3.connect("records.db")
     cur = con.cursor()
 
+    res = cur.execute("SELECT CaseNumber from Records WHERE SummonsText is not null")
+    processed_cases = [c[0] for c in res.fetchall()] 
+
+    case_numbers = [c for c in case_numbers if c not in processed_cases] + ["15-2-06784-8", "16-2-10260-9", "16-2-10277-3", "16-2-10276-5", "16-2-10269-2", "16-2-10284-6"]
+
+    print("We have", str(len(case_numbers)), "left!")
+
     for c in case_numbers:
 
         print(c)
-        con.execute(f"""INSERT INTO Records (CaseNumber) VALUES (\'{c}\')""")
-        con.commit()
+        res = cur.execute(f"SELECT * FROM Records WHERE CaseNumber='{c}'")
+        
+        if res.fetchone() is None:
+            con.execute(f"INSERT INTO Records (CaseNumber) VALUES (\'{c}\')")
+            con.commit()
+
+        res = cur.execute(f"SELECT SummonsText FROM Records WHERE CaseNumber='{c}'")
 
         try:
 
-            file = open(f"./Records/{c}/{c}.htm",'r')
+            with ZipFile(f"./Records/{c}.zip", mode = "r") as zip_case:
 
-            con.execute(f"UPDATE Records SET HasRecordDownloaded = 1 WHERE CaseNumber = \"{c}\"")
-            con.commit()
+                file = zip_case.open(f"{c}.htm",'r').read()
 
-            soup = BeautifulSoup( file, "html.parser")
-            tds = soup.select('td')
+                con.execute(f"UPDATE Records SET HasRecordDownloaded = 1 WHERE CaseNumber = \"{c}\"")
+                con.commit()
 
-            for td in tds :
+                soup = BeautifulSoup( file, "html.parser" )
+                tds = soup.select('td')
 
-                if re.search("summons", td.text, re.IGNORECASE):
+                for td in tds :
 
-                    summons = td
-                    objs = list(summons.parent)
-                    summons_date = datetime.datetime.strptime(objs[0].text, '%m/%d/%Y').strftime('%Y-%m-%d')
-                    summons_path = objs[1].find_all("a", href=True)[0]["href"]
-                    text_path = re.sub("pdf", "txt", summons_path)
-            
-                    summons_file = open(f"./Records/{c}/{text_path}",'r')
-                    summons_text = summons_file.read()
+                    if re.search("summons", td.text, re.IGNORECASE):
 
-                    summons_text = re.sub(r"\'", "\'\'", summons_text)
-                    summons_text = re.sub(r"\"", "\"\"", summons_text)
-
-                    cur.execute(f"""
-                        UPDATE Records SET
-                            SummonsPath = \"{summons_path}\",
-                            SummonsText = \"{summons_text}\",
-                            SummonsDate = \"{summons_date}\"
-                        WHERE CaseNumber = \"{c}\"
-                        """)
-                    
-                    con.commit()
-
-                if re.search("complaint", td.text, re.IGNORECASE):
-
-                    complaint = td
-                    objs = list(complaint.parent)
-                    complaint_date = datetime.datetime.strptime(objs[0].text, '%m/%d/%Y').strftime('%Y-%m-%d')
-                    complaint_path = objs[1].find_all("a", href=True)[0]["href"]
-                    text_path = re.sub("pdf", "txt", complaint_path)
-            
-                    complaint_file = open(f"./Records/{c}/{text_path}",'r')
-                    complaint_text = complaint_file.read()
-
-                    complaint_text = re.sub(r"\'", "\'\'", complaint_text)
-                    complaint_text = re.sub(r"\"", "\"\"", complaint_text)
-
-                    cur.execute(f"""
-                        UPDATE Records SET
-                            ComplaintPath = \"{complaint_path}\",
-                            ComplaintText = \"{complaint_text}\",
-                            ComplaintDate = \"{complaint_date}\"
-                        WHERE CaseNumber = \"{c}\"
-                        """)
-                    
-                    con.commit()
-
-                if re.search("return on writ", td.text, re.IGNORECASE):
-
-                    writ = td
-                    objs = list(writ.parent)
-                    writ_date = datetime.datetime.strptime(objs[0].text, '%m/%d/%Y').strftime('%Y-%m-%d')
-                    writ_path = objs[1].find_all("a", href=True)[0]["href"]
-                    text_path = re.sub("pdf", "txt", writ_path)
-        
-                    writ_file = open(f"./Records/{c}/{text_path}",'r')
-                    writ_text = writ_file.read()
-
-                    writ_text = re.sub(r"\'", "\'\'", writ_text)
-                    writ_text = re.sub(r"\"", "\"\"", writ_text)
-
-                    cur.execute(f"""
-                        UPDATE Records SET
-                            WritIssued = 1,
-                            WritPath = \"{writ_path}\",
-                            WritText = \"{writ_text}\",
-                            WritDate = \"{writ_date}\"
-                        WHERE CaseNumber = \"{c}\"
-                        """)
-                    
-                    con.commit()
-
-            if len(tds) > 5:
-                last_entry = tds[-5]
-                objs = list(tds[-5].parent)
-
-                last_entry_date = datetime.datetime.strptime(objs[0].text, '%m/%d/%Y').strftime('%Y-%m-%d')
-                last_entry_action = objs[1].text
-
-                cur.execute(f"""
-                        UPDATE Records SET
-                            LastFilingEntry = \"{last_entry_action}\",
-                            LastFilingDate = \"{last_entry_date}\"
-                        WHERE CaseNumber = \"{c}\"
-                        """)
+                        summons = td
+                        objs = list(summons.parent)
+                        summons_date = datetime.datetime.strptime(objs[0].text, '%m/%d/%Y').strftime('%Y-%m-%d')
+                        summons_path = objs[1].find_all("a", href=True)[0]["href"]
+                        summons_path = zip_case.extract(summons_path)
                 
-            con.commit()
+                        summons_text = convert_to_text(summons_path)
+
+                        summons_text = re.sub(r"\'", "\'\'", summons_text)
+                        summons_text = re.sub(r"\"", "\"\"", summons_text)
+
+                        cur.execute(f"""
+                            UPDATE Records SET
+                                SummonsPath = \"{summons_path}\",
+                                SummonsText = \"{summons_text}\",
+                                SummonsDate = \"{summons_date}\"
+                            WHERE CaseNumber = \"{c}\"
+                            """)
+                        
+                        con.commit()
+
+                        os.remove(summons_path)
+
+                    if re.search("complaint", td.text, re.IGNORECASE):
+
+                        complaint = td
+                        objs = list(complaint.parent)
+                        complaint_date = datetime.datetime.strptime(objs[0].text, '%m/%d/%Y').strftime('%Y-%m-%d')
+                        complaint_path = objs[1].find_all("a", href=True)[0]["href"]
+                        complaint_path = zip_case.extract(complaint_path)
+                
+                        complaint_text = convert_to_text(complaint_path)
+                
+                        complaint_text = re.sub(r"\'", "\'\'", complaint_text)
+                        complaint_text = re.sub(r"\"", "\"\"", complaint_text)
+
+                        cur.execute(f"""
+                            UPDATE Records SET
+                                ComplaintPath = \"{complaint_path}\",
+                                ComplaintText = \"{complaint_text}\",
+                                ComplaintDate = \"{complaint_date}\"
+                            WHERE CaseNumber = \"{c}\"
+                            """)
+                        
+                        con.commit()
+
+                        os.remove(complaint_path)
+
+                    if re.search("return on writ", td.text, re.IGNORECASE):
+
+                        writ = td
+                        objs = list(writ.parent)
+                        writ_date = datetime.datetime.strptime(objs[0].text, '%m/%d/%Y').strftime('%Y-%m-%d')
+                        writ_path = objs[1].find_all("a", href=True)[0]["href"]
+                        writ_path = zip_case.extract(writ_path)
+                
+                        writ_text = convert_to_text(writ_path)
+    
+                        writ_text = re.sub(r"\'", "\'\'", writ_text)
+                        writ_text = re.sub(r"\"", "\"\"", writ_text)
+
+                        cur.execute(f"""
+                            UPDATE Records SET
+                                WritIssued = 1,
+                                WritPath = \"{writ_path}\",
+                                WritText = \"{writ_text}\",
+                                WritDate = \"{writ_date}\"
+                            WHERE CaseNumber = \"{c}\"
+                            """)
+                        
+                        con.commit()
+
+                        os.remove(writ_path)
+
+                if len(tds) > 5:
+                    last_entry = tds[-5]
+                    objs = list(tds[-5].parent)
+
+                    last_entry_date = datetime.datetime.strptime(objs[0].text, '%m/%d/%Y').strftime('%Y-%m-%d')
+                    last_entry_action = objs[1].text
+
+                    cur.execute(f"""
+                            UPDATE Records SET
+                                LastFilingEntry = \"{last_entry_action}\",
+                                LastFilingDate = \"{last_entry_date}\"
+                            WHERE CaseNumber = \"{c}\"
+                            """)
+                    
+                con.commit()
 
 
-        except OSError:
+        except:
+
             logging.debug(c)
+
             con.execute(f"UPDATE Records SET HasRecordDownloaded = 0 WHERE CaseNumber = \"{c}\"")
             con.commit()
 
--- ocr.py
+++ ocr.py
@@ -1,11 +1,28 @@
+import pandas as pd
+from bs4 import BeautifulSoup
 import pytesseract
 from pdf2image import convert_from_path
 from PIL.Image import DecompressionBombError
+from zipfile import ZipFile 
 import re
-import multiprocessing
-import glob
 import os
-import pandas as pd
+import sqlite3
+import multiprocessing
+
+def convert_to_text(pdf):
+
+    try:
+        images = convert_from_path(pdf)
+        text = ""
+
+        for image in images:
+            text += pytesseract.image_to_string(image)
+
+        return text
+
+    except DecompressionBombError:
+        with open('ocred.log', "a") as log_file:
+            log_file.write(f"{pdf} is too big!")
 
 def split(a, n):
     k, m = divmod(len(a), n)
@@ -13,44 +30,73 @@ def split(a, n):
 
 def pdf_to_text_ocr(case_number_list):
 
-    for case_number in case_number_list:
+    for c in case_number_list:
 
-        print("working on " + case_number)
+        with ZipFile(f"./Records/{c}.zip", mode = "r") as zip_case:
 
-        pdf_paths = glob.glob("./Records/" + case_number + "/*.pdf")
+            file = zip_case.open(f"{c}.htm",'r').read()
 
-        for pdf_path in pdf_paths:
+            soup = BeautifulSoup( file, "html.parser" )
+            tds = soup.select('td')
 
-            text_file_path = re.sub("pdf", "txt", pdf_path)
+            tds = soup.select('td')
 
-            if not os.path.isfile(text_file_path):
-                try:
-                    images = convert_from_path(pdf_path)
-                    text = ""
-
-                    for image in images:
-                        text += pytesseract.image_to_string(image)
-
-                    with open(text_file_path, "w") as text_file:
-                        text_file.write(text)
+            for td in tds :
                 
-                except DecompressionBombError:
-                    with open('ocred.log', "a") as log_file:
-                        log_file.write(f"{pdf_path} is too big!")
+                if re.search("summons", td.text, re.IGNORECASE):
+
+                    summons = td
+                    objs = list(summons.parent)
+
+                    summons_path = objs[1].find_all("a", href=True)[0]["href"]
+                    summons_path = zip_case.extract(summons_path)
+                    
+                    summons_text = convert_to_text(summons_path)
+
+                    with open(f"./Records/{c}_summons.txt", "w") as summons_file:
+                        summons_file.write(summons_text)
+
+                    os.remove(summons_path)
+
+                if re.search("complaint", td.text, re.IGNORECASE):
+
+                    complaint = td
+                    objs = list(complaint.parent)
+
+                    complaint_path = objs[1].find_all("a", href=True)[0]["href"]
+                    complaint_path = zip_case.extract(complaint_path)
+                    
+                    complaint_text = convert_to_text(complaint_path)
+
+                    with open(f"./Records/{c}_complaint.txt", "w") as complaint_file:
+                        complaint_file.write(complaint_text)
+
+                    os.remove(complaint_path)
+
+                if re.search("return on writ", td.text, re.IGNORECASE):
+
+                    writ = td
+                    objs = list(writ.parent)
+
+                    writ_path = objs[1].find_all("a", href=True)[0]["href"]
+                    writ_path = zip_case.extract(writ_path)
+                    
+                    writ_text = convert_to_text(writ_path)
+
+                    with open(f"./Records/{c}_writ.txt", "w") as writ_file:
+                        writ_file.write(writ_text)
+                    
+                    os.remove(writ_path)
 
 if __name__ == '__main__':
     
     df = pd.read_csv("CaseNumber.csv")
     cases = df["CaseNumber"].to_list()
 
-    cases = [case for case in cases if len(glob.glob("./Records/" + case + "/*.txt")) == 0]
+    processed_cases = [file[:12] for file in os.listdir("./Records") if file.endswith(".txt")]
+    processed_cases = list(set(processed_cases))
 
-    log = open("html_extract.log", "r")
-    # Regex pattern for the exact string
-    pattern = r"\d{2}-\d-\d{5}-\d"
-
-    # Use re.findall to find all matches
-    cases = re.findall(pattern, log.read())
+    cases = [c for c in cases if c not in processed_cases] 
 
     N = 8
 
